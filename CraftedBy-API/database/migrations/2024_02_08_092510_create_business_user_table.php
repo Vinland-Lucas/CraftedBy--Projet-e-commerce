741b94a0bc69ab04394fe496e9c95a0b75285d36
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2024-02-08 12:54:58 +0100

    UPDT - Rename de la table intermédiaire "User_Business" en "Business_User" pour suivre la convention qui dit que les tables intermédiaires doivent être nommées dans l'ordre alphabétique

--- CraftedBy-API/database/migrations/2024_02_08_092510_create_business_user_table.php
+++ CraftedBy-API/database/migrations/2024_02_08_092510_create_business_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_business', function (Blueprint $table) {
+        Schema::create('business_user', function (Blueprint $table) {
             $table->foreignUuid('user_id')->constrained();
             $table->foreignUuid('business_id')->constrained();
         });
