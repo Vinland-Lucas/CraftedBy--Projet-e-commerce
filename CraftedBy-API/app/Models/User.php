b93cc27c735bf75ab82b391dbc2355195ce47cea
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2024-02-12 11:04:15 +0100

    Defining relationship between User & Role Model AND Specify the fillable property

--- CraftedBy-API/app/Models/User.php
+++ CraftedBy-API/app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -15,15 +16,24 @@ class User extends Model
 {
     use HasApiTokens, HasFactory, Notifiable, HasUuids;
 
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'lastname',
+        'firstname',
         'email',
         'password',
+        'address',
+        // 'zip_code_id',
+        // 'city_id'
     ];
 
     /**
